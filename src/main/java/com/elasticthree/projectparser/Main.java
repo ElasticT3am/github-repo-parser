81784881e36b3c063b34efbf1cbfe7dafea7fb3f
Mike Milonakis <michail.milonakis@gmail.com>
2016-09-03 15:08:45 +0300

    Added multithreading support

--- src/main/java/com/elasticthree/projectparser/Main.java
+++ src/main/java/com/elasticthree/projectparser/Main.java
@@ -13,6 +13,8 @@ import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class Main {
@@ -25,25 +27,35 @@ public class Main {
         String pass = line.getOptionValue("password");
         int year = Integer.valueOf(line.getOptionValue("year"));
         File reposDir = ParserFileUtils.createNewDir(System.getProperty("user.home") + "/.repoparser/" + year);
+        ExecutorService executorService = Executors.newFixedThreadPool(31);
 
         for (String dateRange : new ParserDateUtils(year)) {
+            executorService.execute(() -> {
+                Map<String, String> requestParams = new HashMap<>();
+                requestParams.put("language", "Java");
+                requestParams.put("created", dateRange);
+                RepoParser repoParser = null;
+                try {
+                    repoParser = new RepoParser(userName, pass, requestParams);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-            Map<String, String> requestParams = new HashMap<>();
-            requestParams.put("language", "Java");
-            requestParams.put("created", dateRange);
-            RepoParser repoParser = new RepoParser(userName, pass, requestParams);
-
-            for (List<SearchRepository> repoList : repoParser) {
-                for (SearchRepository repo : repoList) {
-                    String repoZipUrl = repoParser.getRepoZipUrl(repo);
-                    try {
-                        Files.write(Paths.get(repoParser.getReposFile().toString()), (repoZipUrl + "\n").getBytes(), StandardOpenOption.APPEND);
-                        FileUtils.copyURLToFile(new URL(repoZipUrl), new File(reposDir, repoZipUrl.replace("https://", "").replace("/", "_")));
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                assert repoParser != null;
+                for (List<SearchRepository> repoList : repoParser) {
+                    assert repoList != null;
+                    for (SearchRepository repo : repoList) {
+                        String repoZipUrl = repoParser.getRepoZipUrl(repo);
+                        try {
+                            Files.write(Paths.get(repoParser.getReposFile().toString()), (repoZipUrl + "\n").getBytes(), StandardOpenOption.APPEND);
+                            FileUtils.copyURLToFile(new URL(repoZipUrl), new File(reposDir, repoZipUrl.replace("https://", "").replace("/", "_")));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-            }
+            });
+
         }
     }
 
